118d6bb111df32623099dd1717b043817c226821
Kiran Shny <ikiran1997@gmail.com>
2026-05-21 02:22:53 +0530

    [Fix] use AGP 9 KMP combined plugin

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,22 +1,19 @@
 plugins {
-    alias(libs.plugins.kotlin.multiplatform)
-    alias(libs.plugins.android.library)
+    alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
-    androidTarget {
-        compilations.all {
-            kotlinOptions {
-                jvmTarget = "11"
-            }
-        }
+    androidLibrary {
+        namespace = "io.github.kiranshny.rhinolens.shared"
+        compileSdk = 36
+        minSdk = 24
     }
 
     listOf(
         iosX64(),
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
     ).forEach { target ->
         target.binaries.framework {
             baseName = "Shared"
@@ -30,21 +27,5 @@ kotlin {
             implementation(libs.kotlinx.datetime)
             implementation(libs.kotlinx.serialization.json)
         }
-    }
-}
-
-android {
-    namespace = "io.github.kiranshny.rhinolens.shared"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
-    defaultConfig {
-        minSdk = 24
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
     }
 }
