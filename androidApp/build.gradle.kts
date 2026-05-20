dc19ae889a3d8f6cb0b2fa0841bf4675e5f3a588
Kiran Shny <ikiran1997@gmail.com>
2026-05-21 02:33:56 +0530

    [Task] add CameraX and Navigation Compose deps

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -67,6 +67,16 @@ dependencies {
 
     implementation(libs.androidx.datastore.preferences)
 
+    implementation(libs.androidx.navigation.compose)
+
+    implementation(libs.androidx.camera.core)
+    implementation(libs.androidx.camera.camera2)
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    implementation(libs.androidx.camera.mlkit.vision)
+
+    implementation(libs.coil.compose)
+
     implementation(libs.mlkit.text.recognition)
     implementation(libs.mlkit.text.recognition.chinese)
     implementation(libs.mlkit.text.recognition.devanagari)
